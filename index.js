0d490d4ea0b5a28b82a0e17d6d32bf77d5dd8e5d
Unknown <prpr48@naver.com>
2021-06-06 23:15:10 +0900

    add sample resolvers

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ const typeDefs = gql`
     books: [Book]
   }
 `;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
