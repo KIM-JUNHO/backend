23b1cc0498f5faf12bc91f26d052c884d2cd1d5f
jh3618-kim <jh3618.kim@samsung.com>
2021-06-07 20:30:40 +0900

    add sample mongodb

--- index.js
+++ index.js
@@ -6,6 +6,31 @@ import { environment } from './environment.js';
 import { typeDefs } from './typeDefs.js';
 import { resolvers } from './resolvers.js';
 
+import mongodb from 'mongodb';
+const { MongoClient } = mongodb;
+
+const uri = 'mongodb://localhost:27017/school';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
