6f8ab78866c4f6058d2732d15b7eac02a49a401e
jh3618-kim <jh3618.kim@samsung.com>
2021-06-08 00:30:37 +0900

    reset basic apollo server express

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const { environment } = require('./environment.js');
+const { typeDefs } = require('./schema.js');
+const { resolvers } = require('./resolvers.js');
+
+async function startApolloServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: context,
+  });
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.use((req, res) => {
+    res.status(200);
+    res.send('Hello!');
+    res.end();
+  });
+
+  await new Promise((resolve) => app.listen({ port: environment.port }, resolve));
+  console.log(`🚀 Server ready at http://localhost:${environment.port}${server.graphqlPath}`);
+  return { server, app };
+}
+
+startApolloServer();
