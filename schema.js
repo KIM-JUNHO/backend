7310b544c77a9d9590302d74ed381247fd81ae58
jh3618-kim <jh3618.kim@samsung.com>
2021-06-07 18:12:52 +0900

    add schema.js

---
+++ schema.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+module.exports = {
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+};
