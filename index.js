721640a669aaf9beb5997a6e67bb7ca61d6d4ebd
jh3618-kim <jh3618.kim@samsung.com>
2021-06-07 20:08:39 +0900

    use mocks for test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { ApolloServer } from 'apollo-server';
 
+import { DateTimeMock, EmailAddressMock, UnsignedIntMock } from 'graphql-scalars';
+
 import { environment } from './environment.js';
 import { typeDefs } from './typeDefs.js';
 import { resolvers } from './resolvers.js';
@@ -7,6 +9,12 @@ import { resolvers } from './resolvers.js';
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  mocks: {
+    DateTime: DateTimeMock,
+    EmailAddress: EmailAddressMock,
+    UnsignedInt: UnsignedIntMock,
+  }, // TODO: Remove in PROD.
+  mockEntireSchema: false, // TODO: Remove in PROD.
 });
 
 server.listen(environment.port).then(({ url }) => {
