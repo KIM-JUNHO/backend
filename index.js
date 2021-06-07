277697508b3d63b80cd0371e53651eb2f988bd67
jh3618-kim <jh3618.kim@samsung.com>
2021-06-07 19:28:51 +0900

    use dotenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { ApolloServer } from 'apollo-server';
-
+import dotenv from 'dotenv';
 import { typeDefs, resolvers } from './schema.js';
 
+dotenv.config();
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
